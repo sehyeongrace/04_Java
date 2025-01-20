c4df6105e9ac697f7b011fde4717b1338f2f3d6c
sehyeongrace <sehyeon.grace@gmail.com>
2025-01-20 11:28:47 +0900

    java workspace 등록

---
+++ 00_JavaTest/src/Test1.java
@@ -0,0 +1,32 @@
+// JS와 주석 동일
+
+/* Run 버튼 클릭 시 (단축키 ctrl + f11)
+ * 1) 자동으로 컴파일 수행(javac ooo.java) 
+ * -> .class 파일(byte code) 생성
+ * 
+ * 2) 컴파일 완료 후 .class 파일 실행 (java class파일명)
+ */
+
+public class Test1 {
+
+	public static void main(String[] args) {
+
+		System.out.println("이클립스 실행 중");
+		System.out.println("자꾸 그러지마....");
+		System.out.println("타자 연습 좀 해!!!!!");
+		System.out.println("으잉!");
+		
+		/*Java API DOC : 
+		 * Java에서 제공해주는 코드의 사용법이 설명 되어있는 문서
+		 * 
+		 * DOC 확인 방법
+		 * 1) google -> java 21 api 검색 
+		 * 
+		 * 2) 궁금한 코드 클릭 -> shift + f2
+		 */
+		
+		
+
+	}
+
+}
